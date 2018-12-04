7ff65457c1aa71cb906a50d343f57a5487fb77ea
Karthik Ramamurthy <y2kkarthik@gmail.com>
2018-12-04 17:04:47 +0100

    Revert "Remove reference"
    This reverts commit a4c63862804935ac48f37ab045f8e173f7fb0706.

--- src/main/java/com/b2/generator/Main.java
+++ src/main/java/com/b2/generator/Main.java
@@ -12,7 +12,6 @@ public class Main {
   public static void main(String[] args) {
     try {
       //String gAscii = FigletFont.convertOneLine("Gresham");
-
       String gAscii = "Gresham";
 
       System.out.print("Working");
